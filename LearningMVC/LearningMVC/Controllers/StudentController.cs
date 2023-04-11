c9d993cd0b04a980d633ffcc8824fa593235bb40
Ajay Paudel <WhoIsAoH>
2023-04-11 09:15:10 +0545

    middle push for web

---
+++ LearningMVC/LearningMVC/Controllers/StudentController.cs
@@ -0,0 +1,19 @@
+﻿using LearningMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearningMVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult StudentDetail()
+        {
+            StudentModel std = new StudentModel();
+            std.FirstName = "John";
+            std.LastName = "Hero";
+            std.Address = "NoWhere";
+            std.Age = "29";
+            std.College = "BMC";
+            return View(std);
+        }
+    }
+}
