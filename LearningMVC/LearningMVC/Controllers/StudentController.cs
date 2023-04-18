05eafc106744bfa2c0f2bd79c6900a4980072196
WhoIsAoH <ajay.paudel1@gmail.com>
2023-04-18 15:23:01 +0545

    week4

--- LearningMVC/LearningMVC/Controllers/StudentController.cs
+++ LearningMVC/LearningMVC/Controllers/StudentController.cs
@@ -15,5 +15,26 @@ namespace LearningMVC.Controllers
             std.College = "BMC";
             return View(std);
         }
+        public IActionResult StudentList()
+        {
+            StudentModel std = new StudentModel();
+            std.FirstName = "John";
+            std.LastName = "Hero";
+            std.Address = "NoWhere";
+            std.Age = "29";
+            std.College = "BMC";
+
+            StudentModel std1 = new StudentModel();
+            std1.FirstName = "test";
+            std1.LastName = "Heasdfro";
+            std1.Address = "NoWhere";
+            std1.Age = "29";
+            std1.College = "BMC";
+
+            List<StudentModel> students = new List<StudentModel>();
+            students.Add(std);
+            students.Add(std1);
+            return View(students);
+        }
     }
 }
