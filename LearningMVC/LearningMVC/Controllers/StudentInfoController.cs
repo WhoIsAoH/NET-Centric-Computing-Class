a025817e0903bc926c6382bc2d457966c41274b1
WhoIsAoH <ajay.paudel1@gmail.com>
2023-04-24 21:45:01 +0545

    playing with database

---
+++ LearningMVC/LearningMVC/Controllers/StudentInfoController.cs
@@ -0,0 +1,30 @@
+﻿using LearningMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using static System.Net.Mime.MediaTypeNames;
+using System.Drawing;
+using System;
+using System.Data.SqlClient;
+
+namespace LearningMVC.Models
+{
+    public class StudentInfoController : Controller
+    {
+        public IActionResult AddStudent()
+        {
+            StudentInfoController stdInfo = new StudentInfoController();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddStudent(StudentInfoModel std)
+        {
+            string ConnectionString = "Data Source = (localdb)\\ProjectModels; Initial Catalog = BMC; Integrated Security = True; Connect Timeout = 30; Encrypt = False;";
+            SqlConnection Connection = new SqlConnection(ConnectionString);
+            Connection.Open();
+            String Command = "Insert into StudentInfo values(2,'melina','csit','china')";
+            SqlCommand cmd = new SqlCommand(Command, Connection);
+            cmd.ExecuteNonQuery();
+            Connection.Close();
+            return View();
+        }
+    }
+}
---
+++ LearningMVC/LearningMVC/Models/StudentInfoModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LearningMVC.Models
+{
+    public class StudentInfoModel
+    {
+        public string StudentName { get; set; }
+        public string Course { get; set; }
+        public string? Address { get; set; }
+    }
+}
